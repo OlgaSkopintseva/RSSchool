c424cecd1fed927cdd609371e05775b492fdc006
Olga Skopintseva <chocolate.olga.05@gmail.com>
2023-09-03 13:17:11 +0300

    feat: add radioButtons.forEach

--- library/src/pages/slider.js
+++ library/src/pages/slider.js
@@ -2,18 +2,39 @@ import { booksArray } from "./constant.js";
 
 const favoritesCards = document.querySelector(".favorites__cards");
 const seasonButtons = document.querySelectorAll(".favorites__season");
+const favoritesSeasonRadio = document.querySelectorAll(
+  ".favorites__season-radio"
+);
 
-seasonButtons.forEach((season) => {
-  season.addEventListener("click", (event) => {
-    if (event.target.id === "favorites__choise_winter") {
-      createCards("winter");
-    } else if (event.target.id === "favorites__choise_spring") {
-      createCards("spring");
-    } else if (event.target.id === "favorites__choise_summer") {
-      createCards("summer");
-    } else if (event.target.id === "favorites__choise_autumn") {
-      createCards("autumn");
-    }
+function clearCheckedAttribute() {
+  for (let index = 1; index < favoritesSeasonRadio.length; index++) {
+    favoritesSeasonRadio[index].checked = false;
+  }
+}
+
+seasonButtons.forEach((seasonButton) => {
+  seasonButton.addEventListener("click", (event) => {
+    // clearCheckedAttribute();
+    const currentTarget = event.currentTarget.className;
+    favoritesCards.style.opacity = "0";
+    favoritesCards.style.visibility = "hidden";
+    setTimeout(() => {
+      if (currentTarget === "favorites__season favorites__season_winter") {
+        createCards("winter");
+      } else if (
+        currentTarget === "favorites__season favorites__season_spring"
+      ) {
+        createCards("spring");
+      } else if (
+        currentTarget === "favorites__season favorites__season_summer"
+      ) {
+        createCards("summer");
+      } else if (
+        currentTarget === "favorites__season favorites__season_autumn"
+      ) {
+        createCards("autumn");
+      }
+    }, 300);
   });
 });
 
@@ -43,6 +64,29 @@ const createCards = (season) => {
   bookCards.forEach((bookCard) => {
     bookCard.remove();
   });
+
+  favoritesCards.style.opacity = "1";
+  favoritesCards.style.visibility = "visible";
 };
 
 createCards("winter");
+
+const radioButtons = document.querySelectorAll(".favorites__season-radio");
+const seasonTitles = document.querySelectorAll(".favorites__season-title");
+let lastCheckedTitle = null;
+
+radioButtons.forEach((radio, index) => {
+  if (radio.checked) {
+    seasonTitles[index].classList.add("favorites__season-title_weight-700");
+    lastCheckedTitle = seasonTitles[index];
+  }
+
+  radio.addEventListener("change", () => {
+    if (radio.checked) {
+      if (lastCheckedTitle)
+        lastCheckedTitle.classList.remove("favorites__season-title_weight-700");
+      seasonTitles[index].classList.add("favorites__season-title_weight-700");
+      lastCheckedTitle = seasonTitles[index];
+    }
+  });
+});
