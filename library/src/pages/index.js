503fb4df6c0fc74aa5295b7b700087158b9b0eaf
Olga Skopintseva <chocolate.olga.05@gmail.com>
2023-09-04 17:15:12 +0300

    feat: add js logic to popup_profile

--- library/src/pages/index.js
+++ library/src/pages/index.js
@@ -4,25 +4,29 @@ const headerIconUser = document.querySelector(".header__profile-icon_user");
 
 const popup = document.querySelector(".popup");
 const popups = document.querySelectorAll(".popup");
-const popupCloseButton = document.querySelector(".popup__close");
-const popupLinks = document.querySelectorAll(".popup__navigation-link");
 
 const popupNoAuth = document.querySelector(".popup_no-auth");
 const popupWithAuth = document.querySelector(".popup_with-auth");
-
+const popupLogin = document.querySelector(".popup_login");
 const popupRegister = document.querySelector(".popup_register");
+const popupUserProfile = document.querySelector(".popup_profile");
+
+const popupLinks = document.querySelectorAll(".popup__navigation-link");
+const popupLinkLogin = document.querySelector(".popup__button-profile_login");
 const popupLinkRegister = document.querySelector(
   ".popup__button-profile_register"
 );
+const popupLinkMyProfile = document.querySelector(".popup__button-profile_my-profile");
+const popupLinkProfileLogout = document.querySelector(".popup__button-profile_log-out");
 const digitalLibrarySignUp = document.querySelector(".digital__button_sign-up");
+const digitalLibraryLogin = document.querySelector(".digital__button_log-in");
+
+const popupCloseButton = document.querySelector(".popup__close");
 const popupRegisterCloseButton = document.querySelector(
   ".popup__close_register"
 );
-
-const popupLogin = document.querySelector(".popup_login");
-const popupLinkLogin = document.querySelector(".popup__button-profile_login");
-const digitalLibraryLogin = document.querySelector(".digital__button_log-in");
 const popupLoginCloseButton = document.querySelector(".popup__close_login");
+const popupProfileClose = document.querySelector(".popup__close_profile");
 
 function closeAllPopups() {
   popups.forEach((popup) => {
@@ -162,4 +166,24 @@ popupWithAuth.addEventListener("click", (event) => {
   if (event.target.classList.contains("popup_with-auth")) {
     popupWithAuth.classList.remove("popup_enable");
   }
+});
+
+// click on My profile button //
+popupLinkMyProfile.addEventListener("click", () => {
+  popupUserProfile.classList.add("popup_enable");
+});
+
+popupProfileClose.addEventListener("click", () => {
+  popupUserProfile.classList.remove("popup_enable")
+});
+
+popupUserProfile.addEventListener("click", (event) => {
+  if (event.target.classList.contains("popup_profile")) {
+    popupUserProfile.classList.remove("popup_enable");
+  }
+});
+
+popupLinkMyProfile.addEventListener("click", () => {
+  popupWithAuth.classList.remove("popup_enable");
+  popupUserProfile.classList.add("popup_enable");
 });
