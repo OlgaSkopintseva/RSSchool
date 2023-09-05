99f42a5e8b35017c79d69351d25fa52fe87eb7d1
Olga Skopintseva <chocolate.olga.05@gmail.com>
2023-09-05 11:03:29 +0300

    feat: add iconProfile.textContent

--- library/src/pages/localStorage.js
+++ library/src/pages/localStorage.js
@@ -1,4 +1,5 @@
 const popupRegisterForm = document.querySelector(".popup__form_register");
+const iconProfile = document.querySelector(".header__profile-icon");
 
 popupRegisterForm.addEventListener("submit", function () {
   const registerFirstName = popupRegisterForm.querySelector(
@@ -20,6 +21,9 @@ popupRegisterForm.addEventListener("submit", function () {
   };
 
   localStorage.setItem("popupRegisterForm", JSON.stringify(formRegisterData));
+
+  iconProfile.classList.add("header__profile-icon_user");
+  iconProfile.textContent = registerFirstName.slice(0, 1) + registerLastName.slice(0, 1)
 });
 
 const popupLoginForm = document.querySelector(".popup__form_login");
@@ -30,7 +34,7 @@ popupLoginForm.addEventListener("submit", function () {
 
   const formLoginData = {
     email: loginEmail,
-    password: loginPassword
+    password: loginPassword,
   };
 
   localStorage.setItem("popupLoginForm", JSON.stringify(formLoginData));
